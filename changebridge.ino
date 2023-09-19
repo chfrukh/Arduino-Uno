28ee364ab5e9a6ff449a3d4de2ef7b1aac9c7b58
Ch Farrukh Rafique <56925356+chfrukh@users.noreply.github.com>
2023-09-19 19:23:10 +0100

    Add files via upload

---
+++ changebridge.ino
@@ -0,0 +1,18 @@
+void setup () {
+  pinMode(1, OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+}
+
+void loop() {
+//peripheral_loop();
+digitalWrite(1,HIGH);
+digitalWrite(3,HIGH);
+delay(1000);
+digitalWrite(1,LOW);
+digitalWrite(3,LOW);
+digitalWrite(2,HIGH);
+digitalWrite(4,HIGH);
+delay(1000);
+}
