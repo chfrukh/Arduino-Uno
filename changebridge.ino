2d8d3cba29abb5e3bc5b74cabeb5d456b97fa564
Ch Farrukh Rafique <56925356+chfrukh@users.noreply.github.com>
2023-09-19 19:24:39 +0100

    Update changebridge.ino

--- changebridge.ino
+++ changebridge.ino
@@ -1,18 +1,27 @@
-void setup () {
+void setup() {
+  // Configure pins 1, 2, 3, and 4 as OUTPUTs
   pinMode(1, OUTPUT);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-//peripheral_loop();
-digitalWrite(1,HIGH);
-digitalWrite(3,HIGH);
-delay(1000);
-digitalWrite(1,LOW);
-digitalWrite(3,LOW);
-digitalWrite(2,HIGH);
-digitalWrite(4,HIGH);
-delay(1000);
+  // Turn ON pins 1 and 3
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+
+  // Wait for 1 second
+  delay(1000);
+
+  // Turn OFF pins 1 and 3
+  digitalWrite(1, LOW);
+  digitalWrite(3, LOW);
+
+  // Turn ON pins 2 and 4
+  digitalWrite(2, HIGH);
+  digitalWrite(4, HIGH);
+
+  // Wait for 1 second
+  delay(1000);
 }
